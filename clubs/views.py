c83ee10f3a21303bcbea7efcdf9e17e5feba73e9
fandasman <f.hermanek@gmail.com>
2021-11-24 21:22:29 +0000

    Add placeholder main page 'home' view.

--- clubs/views.py
+++ clubs/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.contrib.auth import get_user_model, authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from django.urls import reverse
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
+# Create the main page view
+def home(request):
+    return render(request, 'home.html')

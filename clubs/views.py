477c1913426352942a394ba9bd57bcd72cde3278
Roger20069769 <81314310+Roger20069769@users.noreply.github.com>
2021-12-01 19:38:09 +0000

    add log in views in view

--- clubs/views.py
+++ clubs/views.py
@@ -12,3 +12,18 @@ from django.core.exceptions import ObjectDoesNotExist
 # Create the main page view
 def home(request):
     return render(request, 'home.html')
+
+
+def log_in(request):
+    # if request.method == 'POST':
+    #     form = LogInForm(request.POST)
+    #     if form.is_valid():
+    #         username = form.cleaned_data.get('username')
+    #         password = form.cleaned_data.get('password')
+    #         user = authenticate(username=username, password=password)
+    #         if user is not None:
+    #             login(request, user)
+    #             return redirect('feed')
+    #     messages.add_message(request, messages.ERROR, "The credentials provided were invalid!")
+    form = LogInForm()
+    return render(request, 'log_in.html', {'form': form})

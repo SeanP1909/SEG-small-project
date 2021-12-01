0db8c5848c6ced762741a3cc7143c81e727dfe34
Ivan <k20011632@kcl.ac.uk>
2021-12-01 21:00:48 +0000

    Add password and username constraints.

--- clubs/forms.py
+++ clubs/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.validators import RegexValidator
 from .models import User
 
 class SignUpForm(forms.ModelForm):
@@ -7,5 +8,21 @@ class SignUpForm(forms.ModelForm):
         fields = ["first_name", "last_name", "username", "email", "experience", "bio"]
         widgets = { "bio": forms.Textarea() }
 
-    new_password = forms.CharField(label="Password", widget=forms.PasswordInput())
+    new_password = forms.CharField(
+        label="Password", 
+        widget=forms.PasswordInput(),
+        validators=[
+            RegexValidator(
+                regex = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$',
+                message = 'The password must contain an uppercase, lowercase and numeric characters!'
+                )
+        ]
+    )
     password_confirmation = forms.CharField(label="Confirm Password", widget=forms.PasswordInput())
+
+    def clean(self):
+        super().clean()
+        new_password = self.cleaned_data.get('new_password')
+        password_confirmation = self.cleaned_data.get('password_confirmation')
+        if new_password != password_confirmation:
+            self.add_error('password_confirmation', 'The confirmation does not match the original password.')

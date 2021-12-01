cc167a10083120cd3a4f45904159771baa89ff3a
Ivan <k20011632@kcl.ac.uk>
2021-12-01 18:28:19 +0000

    Create the sign up form, view and link the url to the main page

--- clubs/models.py
+++ clubs/models.py
@@ -2,19 +2,13 @@ from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from libgravatar import Gravatar
+from .helpers import experienceChoices
 
 # Create your models here.
 
 # Create the User model
 class User(AbstractUser):
-    EXPERIENCE_LEVELS = (
-        ("beginner", "Beginner"),
-        ("intermediate", "Intermediate"),
-        ("advanced", "Advanced"),
-        ("master", "Master"),
-        ("professional", "Professional")
-    )
-
+    #User model atributes.
     username = models.CharField(
         max_length = 30,
         unique=True
@@ -22,7 +16,7 @@ class User(AbstractUser):
     first_name = models.CharField(max_length = 50)
     last_name = models.CharField(max_length = 50)
     email = models.EmailField(unique = True, blank = False)
-    experience = models.CharField(max_length=12, choices=EXPERIENCE_LEVELS, default='beginner')
+    experience = models.CharField(max_length=12, choices=experienceChoices())
     bio = models.CharField(max_length = 520, blank = True)
 
     def full_name(self):
--- clubs/views.py
+++ clubs/views.py
@@ -8,7 +8,12 @@ from django.http import HttpResponseForbidden
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import ObjectDoesNotExist
+from .forms import SignUpForm
 
 # Create the main page view
 def home(request):
     return render(request, 'home.html')
+
+def sign_up(request):
+    form = SignUpForm()
+    return render(request, 'sign_up.html', {'form': form})

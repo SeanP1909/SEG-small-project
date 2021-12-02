81bbd9d20a3ab4a3dafa5337f4b32a5f2da3e1df
Roger20069769 <81314310+Roger20069769@users.noreply.github.com>
2021-12-02 22:41:52 +0000

    Update views.py

--- clubs/views.py
+++ clubs/views.py
@@ -8,10 +8,22 @@ from django.http import HttpResponseForbidden
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import ObjectDoesNotExist
+from .forms import SignUpForm
 
 # Create the main page view
 def home(request):
     return render(request, 'home.html')
+
+def sign_up(request):
+    if request.method=='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'sign_up.html', {'form': form})
+
 
 
 def log_in(request):

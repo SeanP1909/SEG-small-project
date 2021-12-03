4b0411ead33c676f360f239e2fbdd0b4a3c73859
fandasman <f.hermanek@gmail.com>
2021-12-03 17:58:48 +0000

    Update seed and unseed commands for clubs
    - Update seed command to generate 10 fake clubs
    - Update unseed command to delete ALL clubs
    - Add clubs to admin interface

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,10 +1,17 @@
 """Cofiguration of the admin interface for microblogs."""
 from django.contrib import admin
-from .models import User
+from .models import User, Club
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     """Configuration of the admin interface for users."""
     list_display = [
         'username', 'first_name', 'last_name', 'email', 'is_active',
+    ]
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for users."""
+    list_display = [
+        'name', 'location', 'description',
     ]

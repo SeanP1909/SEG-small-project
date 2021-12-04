21c1702ecec4b644a9a297739a24f653e5496908
Ivan <k20011632@kcl.ac.uk>
2021-12-04 22:48:45 +0000

    Create unit test for the club member model.

--- clubs/tests/models/test_club_member_model.py
+++ clubs/tests/models/test_club_member_model.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
-from clubs.models import Club
+from clubs.models import User, Club, ClubMember
 
 # Create your tests here.
 class ClubMemberModelTestCase(TestCase):
@@ -14,21 +14,33 @@ class ClubMemberModelTestCase(TestCase):
                 'clubs/tests/fixtures/other_clubmembers.json']
 
     def setUp(self):
+        self.user = User.objects.get(username = 'johndoe')
+        self.club = Club.objects.get(name = 'ChessHub')
         self.clubmember = ClubMember.objects.get(
             username = 'johndoe',
             name = 'ChessHub'
         )
 
-# Test
+# Test club member must be a unique entry.
     def test_club_member_must_be_unique(self):
         second_club_member = ClubMember.objects.get(
             username='janedoe',
             name='Chessy')
-        if self.clubmember.username = second_club_member.username && self.clubmember.name = second_club_member.name:
-            self._assert_club_member_is_invalid()
+        self.clubmember = second_club_member
+        self._assert_club_member_is_invalid()
 
 # Test 
-        
+    def test_club_member_Table_is_deleted_upon_deleting_the_club_model(self):
+        self.club.delete()
+        with self.assertRaises(ClubMember.DoesNotExist):
+            ClubMember.objects.get(pk = self.clubmember.pk)
+        self._assert_club_member_is_invalid()
+
+    def test_club_member_table_is_deleted_upon_deleting_the_user_model(self):
+        self.user.delete()
+        with self.assertRaises(ClubMember.DoesNotExist):
+            ClubMember.objects.get(pk = self.clubmember.pk)
+        self._assert_club_member_is_invalid()
 
 # Test case assertions
     def _assert_club_member_is_valid(self):

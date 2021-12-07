c80bd1f4a19ec1c36ba8887d03a644c0c0ab5a36
inarvos <inarvos@inarvos-mbp.home>
2021-12-07 20:14:14 +0000

    Added tests for Clubs List view.

---
+++ clubs/tests/views/test_clubs_list_view.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from clubs.models import Club
+
+class ClubListTest(TestCase):
+
+    fixtures = [
+        'clubs/tests/fixtures/default_club.json'
+    ]
+
+    def setUp(self):
+        self.response = self.client.get('/clubs/', {'clubs':Club.objects.all()})
+
+    def test_url_clubs_existence(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_clubs_content_contains_ChessHub(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertContains(self.response, 'ChessHub')
+        self.assertTemplateUsed(self.response, 'clubs.html')
+
+    def test_if_clubs_content_contains_chessList_header(self):
+        self.assertContains(self.response, 'Clubs list:')
+
+    def test_clubs1_url_does_not_exist_responses_404(self):
+        self.assertEqual(self.client.get('/clubs1/').status_code, 404)
